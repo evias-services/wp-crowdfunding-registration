5ca17ef8ff27a23996d69261693abae78b24c1f4
Rebecca N <natterer.re@gmail.com>
2020-03-31 01:16:12 +0000

    added email verification

--- shortcode/RegistrationCustom.php
+++ shortcode/RegistrationCustom.php
@@ -12,15 +12,42 @@ class RegistrationCustom extends Registration {
         
         add_action( 'wp_ajax_wpcf_registration_custom', array( $this, 'registration_save_action' ) );
         add_action( 'wp_ajax_nopriv_wpcf_registration_custom', array( $this, 'registration_save_action' ) );
+        add_action( 'cfreg_verification_email', array( $this, 'send_verification_email' ) );
     }
   
   
     public function registration_callback() {
         ob_start();
-        if ( is_user_logged_in() ) : ?>
-            <h3 class="wpneo-center"><?php _e("You are already logged in.","wp-crowdfunding"); ?></h3>
-        <?php else :
-          global $reg_errors,$reg_success;
+        if ( is_user_logged_in() ) : 
+            $is_verified = get_user_meta(get_user_id(), 'is_activated');
+            if (! $is_verified) :
+            ?>
+            
+            <h3 class="wpneo-center"><?php _e("We have sent you a verification e-mail. Please open it and click the link to verify your e-mail address.","wp-crowdfunding-registration"); ?></h3>
+
+            <?php
+            else : ?>
+
+            <h3 class="wpneo-center"><?php _e("You are already logged in.","wp-crowdfunding-registration"); ?></h3>
+
+            <?php
+            endif; ?>
+
+        <?php 
+        else :
+            global $reg_errors,$reg_success;
+        
+            if(isset($_GET['act'])){
+                $data = unserialize(base64_decode($_GET['act']));
+                $code = get_user_meta($data['id'], 'activation_code', true);
+
+                // verify whether the code given is the same as ours
+                if($code === $data['code']){
+                    // update the user meta
+                    update_user_meta($data['id'], 'is_activated', 1);
+                    wc_add_notice( __( '<strong>Success:</strong> Your account has been activated! ', 'wp-crowdfunding-registration' )  );
+                }
+            }
           ?>
             <div class="wpneo-user-registration-wrap">
                 <form action="<?php echo esc_url($_SERVER['REQUEST_URI']); ?>" id="wpneo-registration" method="post">
@@ -96,16 +123,35 @@ class RegistrationCustom extends Registration {
                             'warpclass'     => '',
                             'autocomplete'  => 'off',
                         ),
+                        array(
+                            'id'            => 'terms_and_conditions',
+                            'label'         => __( "I have read and accept the Terms & Conditions" , "wp-crowdfunding-registration" ),
+                            'type'          => 'checkbox',
+                            'value'         => '',
+                            'warpclass'     => '',
+                            'class'         => 'required',
+                            'autocomplete'  => 'off',
+                        ),
                     );
     
                     $regisration_meta = apply_filters('wpcf_user_registration_fields', $regisration_data );
     
                     foreach( $regisration_meta as $item ){ ?>
                         <div class="wpneo-single <?php echo (isset($item['warpclass'])? $item['warpclass'] : "" ); ?>">
+                          
+                            <?php if ($item['type'] === 'checkbox'): ?>
+                            <div class="wpneo-name">
+                              <input type="checkbox" id="<?php echo $item['id']; ?>" class="<?php echo $item['class']; ?>" name="<?php echo $item['id']; ?>">&nbsp;<?php echo $item['label']; ?>
+                            </div>
+                            <div class="wpneo-fields">&nbsp;</div>
+                            <?php else: ?>
                             <div class="wpneo-name"><?php echo (isset($item['label'])? $item['label'] : "" ); ?></div>
                             <div class="wpneo-fields">
                                 <?php
                                 switch ($item['type']){
+                                    case 'checkbox':
+                                    echo '<label><input type="checkbox" id="'.$item['id'].'" class="'.$item['class'].'" name="'.$item['id'].'">&nbsp;'.$item['label'].'</label>';
+                                        break;
                                     case 'text':
                                     echo '<input type="text" id="'.$item['id'].'" autocomplete="'.$item['autocomplete'].'" class="'.$item['class'].'" name="'.$item['id'].'" placeholder="'.$item['placeholder'].'">';
                                         break;
@@ -123,6 +169,8 @@ class RegistrationCustom extends Registration {
                                         break;
                                 } ?>
                             </div>
+                            <?php endif; ?>
+
                         </div>
                     <?php } ?>
     
@@ -196,11 +244,12 @@ class RegistrationCustom extends Registration {
         $lname  =   sanitize_text_field($_POST['lname']);
         $slname  =   sanitize_text_field($_POST['slname']);
         $phone  =   sanitize_text_field($_POST['phone']);
-        $this->custom_registration_validation( $fname , $lname, $slname, $password , $rpassword, $email, $phone );
-        $this->custom_complete_registration( $fname , $lname, $slname, $password , $rpassword, $email, $phone );
+        $terms  = sanitize_text_field($_POST['terms_and_conditions']);
+        $this->custom_registration_validation( $fname , $lname, $slname, $password , $rpassword, $email, $phone, $terms );
+        $this->custom_complete_registration( $fname , $lname, $slname, $password , $rpassword, $email, $phone, $terms );
     }
     
-    public function custom_complete_registration( $fname , $lname, $slname, $password , $rpassword, $email, $phone ) {
+    public function custom_complete_registration( $fname , $lname, $slname, $password , $rpassword, $email, $phone, $terms ) {
         global $reg_errors;
         if ( count($reg_errors->get_error_messages()) < 1 ) {
             $login = strtolower($fname) . '-' . strtolower($lname) . rand(1, 10000);
@@ -220,6 +269,7 @@ class RegistrationCustom extends Registration {
             if ( !is_wp_error( $user_id ) ) {
                 WC()->mailer(); // load email classes
                 do_action( 'wpcf_after_registration', $user_id );
+                do_action( 'cfreg_verification_email', $user_id );
     
                 $saved_redirect_uri = get_option('wpcf_user_reg_success_redirect_uri');
                 $redirect = $saved_redirect_uri ? $saved_redirect_uri : esc_url( home_url( '/iniciar-sesion' ) );
@@ -244,12 +294,16 @@ class RegistrationCustom extends Registration {
         }
     }
     
-    public function custom_registration_validation( $fname , $lname, $slname, $password , $rpassword, $email, $phone ) {
+    public function custom_registration_validation( $fname , $lname, $slname, $password , $rpassword, $email, $phone, $terms ) {
         global $reg_errors;
         $reg_errors = new \WP_Error;
     
         if ( empty( $rpassword ) || empty( $password ) || empty( $email ) || empty( $fname ) || empty( $lname ) ) {
             $reg_errors->add('field', __('Required form field is missing','wp-crowdfunding'));
+        }
+      
+        if ( empty( $terms )) {
+            $reg_errors->add('field', __('Please, accept the Terms & Conditions','wp-crowdfunding-registration'));
         }
     
         if ( strlen( $password ) < 6 ) {
@@ -266,5 +320,28 @@ class RegistrationCustom extends Registration {
         if ( email_exists( $email ) ) {
             $reg_errors->add('email', __('Email Already in use','wp-crowdfunding'));
         }
+    }
+  
+    public function send_verification_email($user_id) {
+        // get user data
+        $user_info = get_userdata($user_id);
+        // create md5 code to verify later
+        $code = md5(time());
+        // make it into a code to send it to user via email
+        $string = array('id'=>$user_id, 'code'=>$code);
+        // create the activation code and activation status
+        update_user_meta($user_id, 'account_activated', 0);
+        update_user_meta($user_id, 'activation_code', $code);
+        // create the url
+        $url = get_site_url(). '/registration-form/?act=' .base64_encode( serialize($string));
+        // basically we will edit here to make this nicer
+        $html = '
+            Welcome to MundoRaiz!<br/><br/>
+            Please click the following link to proceed with the verification of your e-mail address:
+            <a href="'.$url.'">'.$url.'</a>
+        ';
+
+        // send an email out to user
+        wp_mail( $user_info->user_email, __('Confirma su registracion con MundoRaiz','wp-crowdfunding-registration') , $html);
     }
 }
